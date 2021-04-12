80830f6a7f215acd43b9a7c394aa0e6931df9331
DrSavauG <dr.savaug@gmail.com>
2021-04-12 22:50:55 +0300

    feat: 11 encode line

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,29 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const result = [];
+  let number = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      number += 2;
+      continue;
+      // break;
+    }
+    //  else {
+    result.push(number);
+    result.push(str[i]);
+    number = '';
+    // }
+  }
+  // if (str[i] !== str[i + 1]) {
+  //   result.push(str[i]);
+  // } else {
+  //   result.push(number);
+  // }
+  // }
+  return result.join('');
 }
-
+let str = 'abbcca'; //'4a4t'
+console.log(encodeLine(str));
 module.exports = encodeLine;
