e992ca37a3ce18f6c3e3a681db1a5eede5312ea6
DrSavauG <dr.savaug@gmail.com>
2021-04-12 14:08:54 +0300

    refactor: rm comments

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -11,28 +11,12 @@
  */
 function getCommonCharacterCount(s1, s2) {
   let [result, b] = [0, s2];
-
-  // let result = 0;
-  // // console.log(a);
-  // // console.log(b);
-  // // throw new Error('Not implemented');
-  // // let result = 0;
-  // // console.log([...s1]);
   for (let i = 0; i < s1.length; i++) {
     if (b.includes(s1[i])) {
-      // console.log([b.indexOf(a[i])],i);
       b = b.replace(b[b.indexOf(s1[i])], '');
-      // console.log(b);
       result++;
     }
   }
-
   return result;
-  // console.log(a[1]);
-  // console.log(b.includes(a[1]));
-  //   a
 }
-// const s1 = 'aabcce';
-// const s2 = 'adcaaee';
-// console.log(getCommonCharacterCount(s1, s2));
 module.exports = getCommonCharacterCount;
