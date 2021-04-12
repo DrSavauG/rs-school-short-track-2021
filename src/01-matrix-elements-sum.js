6ef2159a448f79fd12805fb662d15562aff4ab27
DrSavauG <dr.savaug@gmail.com>
2021-04-12 13:23:12 +0300

    refactor: rm comments

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -15,23 +15,14 @@
  * The result should be 9
  */
 function getMatrixElementsSum(matrix) {
-  // return (matrix.length);
-  // if (matrix.length = 1) return matrix[0].reduce((a, b) => a + b, 0);
   let result = 0;
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[i].length; j++) {
       if (i === 0) {
         result += matrix[i][j];
-      } else if ((matrix[i - 1][j] !== 0)) result += matrix[i][j];
+      } else if (matrix[i - 1][j] !== 0) result += matrix[i][j];
     }
   }
   return result;
 }
-
-// let matrix = [
-//   [0, 1, 1, 2],
-//   [0, 5, 0, 0],
-//   [2, 0, 3, 3],
-// ];
-// console.log(getMatrixElementsSum(matrix));
 module.exports = getMatrixElementsSum;
