18f5822dd802e98f0457e376ecfb99d6a05eb599
DrSavauG <dr.savaug@gmail.com>
2021-04-13 00:36:45 +0300

    feat: add 14 queue

--- src/14-queue.js
+++ src/14-queue.js
@@ -1,4 +1,4 @@
-// const ListNode = require('../extensions/list-node');
+const ListNode = require('../extensions/list-node');
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
  *
@@ -12,17 +12,38 @@
  */
 
 class Queue {
-  get size() {
-    throw new Error('Not implemented');
+  constructor() {
+    this.length = 0;
+    this.head = undefined;
+    this.tail = undefined;
   }
 
-  enqueue(/* element */) {
-    throw new Error('Not implemented');
+  get size() {
+    return this.length;
+  }
+
+  enqueue(element) {
+    const node = new ListNode(element);
+    this.length++;
+    if (this.head === undefined) {
+      this.head = node;
+    } else {
+      this.tail.next = node;
+    }
+    this.tail = node;
   }
 
   dequeue() {
-    throw new Error('Not implemented');
+    const temp = this.head;
+    this.head = temp.next;
+    return temp.value;
   }
 }
+// const queue = new Queue();
+// queue.enqueue(5);
+// queue.enqueue(4);
+// queue.enqueue(3);
+// queue.dequeue();
+// console.log(queue)
 
 module.exports = Queue;
